b0889585bb832fd76c840486ae5a614eee0b685e
irkus1911 <iirkus1911@irkusfuga@gmail.com>
2021-12-02 10:32:28 +0100

    PersonalResource entity Terminado

---
+++ src/java/kedamosServerSide/entities/Type.java
@@ -0,0 +1,10 @@
+
+package kedamosServerSide.entities;
+
+/**
+ *Esta clase es la enumeracion la cual especifica el tipo de personal de cada evento
+ * @author Irkus de la Fuente
+ */
+public enum Type {
+    Camarero,Seguridad,Dj,Payaso,Mago,Humorista,Cocinero,Guia,Arbitro,Coctelero,Cantante,Musico,Actor
+}
