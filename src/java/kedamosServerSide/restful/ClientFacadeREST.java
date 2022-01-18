3c035e001373da1d154393980967e320bf75a139
Bot-Steven <83646314+Bot-Steven@users.noreply.github.com>
2022-01-18 08:27:06 +0100

    Se ha borrado las imports que no se usan

--- src/java/kedamosServerSide/restful/ClientFacadeREST.java
+++ src/java/kedamosServerSide/restful/ClientFacadeREST.java
@@ -12,8 +12,6 @@ import javax.persistence.PersistenceContext;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.NotAuthorizedException;
-import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -21,7 +19,6 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import kedamosServerSide.entities.Client;
-import kedamosServerSide.security.Crypt;
 
 /**
  *
